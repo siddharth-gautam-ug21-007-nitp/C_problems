14f22a985f1cc440c5234908e59b92787132d338
siddharth-gautam-ug21-007-nitp <113232058+siddharth-gautam-ug21-007-nitp@users.noreply.github.com>
2022-11-13 15:16:55 +0530

    Added functionality of multiplication and division

--- Aritmetic_operation.c
+++ Aritmetic_operation.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 int main(){
-    int a,b,c=0,d=0;
+    int a,b,c=0,d=0,e=0,f=0;
 
     printf("enter any two number: ");
     scanf("%d %d",&a,&b);
 
     c=a+b;
     d=a-b;
+    e=a*b;
+    f=a/b;
     
     printf("sum is %d \n",c);
     printf("sub is %d \n",d);
+    printf("Multiplication is %d \n",e);
+    printf("Division is %d \n",f);
     return 0;
 }
